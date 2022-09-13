358037cdd4234ed5e833e1506bfa36a0415f912b
DemoGit <haipxph26772@fpt.edu.vn>
2022-09-13 21:55:07 +0700

    add file hai

--- src/demousegitduanhihi/hai.java
+++ src/demousegitduanhihi/hai.java
@@ -1,14 +1,6 @@
 package demousegitduanhihi;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author window
- */
 public class hai {
     public static void main(String[] args) {
         System.out.println("Xin chao toi ten la Hai");

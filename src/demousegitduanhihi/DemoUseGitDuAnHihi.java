278c5b30e6801c1debeb50348b77e5e4d5556615
thangbodoii <thangnvph26792@fpt.edu.vn>
2022-09-13 21:46:58 +0700

    Update Demo class

--- src/demousegitduanhihi/DemoUseGitDuAnHihi.java
+++ src/demousegitduanhihi/DemoUseGitDuAnHihi.java
@@ -19,6 +19,7 @@ public class DemoUseGitDuAnHihi {
         System.out.println("Giờ thì bạn hãy tạo mới file \n");
         System.out.println("Rồi sau đó commit lên, rồi push \n Là bạn đã hoàn thành rồi");
         System.out.println("Hello a thang day");
+        System.out.println("kakakakaka ");
     }
     
 }

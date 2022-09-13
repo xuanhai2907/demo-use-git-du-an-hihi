9e67e475c10dfd57373fc988b02fac8b1a55966e
DemoGit <haipxph26772@fpt.edu.vn>
2022-09-13 21:50:26 +0700

    add file hai

--- src/demousegitduanhihi/hai.java
+++ src/demousegitduanhihi/hai.java
@@ -1,17 +1,16 @@
+package demousegitduanhihi;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package demousegitduanhihi;
 
 /**
  *
  * @author window
  */
-public class demo {
-      public static void main(String[] args) {
-        
-          System.out.println("hihi");
-          System.out.println("hahahahaha");
+public class hai {
+    public static void main(String[] args) {
+        System.out.println("Xin chao toi ten la Hai");
     }
 }

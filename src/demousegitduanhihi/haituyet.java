192bd8cc855e0dd3bfabfe3d872a49c731a7478c
DemoGit <haipxph26772@fpt.edu.vn>
2022-09-14 21:58:40 +0700

    add haiyeutuyet

--- src/demousegitduanhihi/haituyet.java
+++ src/demousegitduanhihi/haituyet.java
@@ -11,5 +11,6 @@ package demousegitduanhihi;
 public class haituyet {
     public static void main(String[] args) {
         System.out.println("hhhhhhhhhhhhh");
+        System.out.println("hai yeu tuyet");
     }
 }
